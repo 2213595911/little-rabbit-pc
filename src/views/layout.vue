e384355fdf46786793f97288724319982d98643b
HaoNan <2213595911@qq.com>
2021-10-31 15:57:23 +0800

    顶部导航通栏

--- src/views/layout.vue
+++ src/views/layout.vue
@@ -1,6 +1,7 @@
 <template>
     <!-- 顶部通栏 -->
-    <nav>顶部通栏</nav>
+    <!-- <nav>顶部通栏</nav> -->
+    <AppTopnav />
     <!-- 头部组件 -->
     <header>头部组件</header>
     <!-- 内容容器 -->
@@ -11,8 +12,10 @@
     <footer>底部组件</footer>
 </template>
 <script>
+import AppTopnav from "@/components/layout-navbar";
 export default {
-  name: 'Layout'
-}
+    name: "Layout",
+    components: { AppTopnav },
+};
 </script>
 <style lang="less"></style>
