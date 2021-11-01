e8788b41e4c8347e28eff1f3a0cd72b21104d3bc
HaoNan <2213595911@qq.com>
2021-11-01 16:40:47 +0800

    完成主体左侧分类以及骨架框

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,14 +1,15 @@
 <template>
-    <div class="container">我是homepage组件</div>
+    <div class="container">
+        <!-- 左侧分类菜单组件 -->
+        <HomeCategory />
+        <!-- 轮播图组件 -->
+    </div>
 </template>
 <script>
+import HomeCategory from "./components/home-category";
 export default {
     name: "Home",
+    components: { HomeCategory },
 };
 </script>
-<style lang="less" scoped>
-.container {
-    .hoverShadow();
-    color: @xtxColor;
-}
-</style>
+<style lang="less" scoped></style>
