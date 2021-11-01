f56738df3143aedb5d03b2e43ae80eeb1cce4966
HaoNan <2213595911@qq.com>
2021-11-01 08:51:24 +0800

    修改了错误的文件名

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -3,6 +3,8 @@
     <AppTopnav />
     <!-- 头部组件 -->
     <AppHeader />
+    <!-- 吸顶导航组件 -->
+    <AppHeaderNav />
     <!-- 内容容器 -->
     <div class="main">
         <router-view></router-view>
@@ -14,10 +16,11 @@
 import AppTopnav from "@/components/layout-navbar";
 import AppHeader from "@/components/layout-header";
 import AppFooter from "@/components/layout-footer";
+import AppHeaderNav from "@/components/layout-header-sticky";
 import { useStore } from "vuex";
 export default {
     name: "Layout",
-    components: { AppTopnav, AppHeader, AppFooter },
+    components: { AppTopnav, AppHeader, AppFooter, AppHeaderNav },
     setup() {
         const store = useStore();
         store.dispatch("category/getCategoryList");
